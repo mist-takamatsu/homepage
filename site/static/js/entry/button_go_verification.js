d9b73beb6ae042ddfb023c85553d11db2cb58118
gnshd <gdev@nshd.info>
2020-03-30 12:47:38 +0900

    Update 確認画面入力チェック

--- site/static/js/entry/button_go_verification.js
+++ site/static/js/entry/button_go_verification.js
@@ -1,9 +1,7 @@
-document.getElementById("button_go_verification").onclick = function() {
+document.getElementById("button_go_verification").onclick = function () {
 	const name = document.entry.name.value;
 	const name_kn = document.entry.name_kn.value;
-	const birth_y = document.entry.birth_y.value;
-	const birth_m = document.entry.birth_m.value;
-	const birth_d = document.entry.birth_d.value;
+	const birth_date = document.entry.birth_date.value;
 	const gender = document.entry.gender.value;
 	const address_code = document.entry.address_code.value;
 	const address_ken = document.entry.address_ken.value;
@@ -13,15 +11,14 @@ document.getElementById("button_go_verification").onclick = function() {
 	const email = document.entry.email.value;
 	const school_name = document.entry.school_name.value;
 	const shcool_faculty = document.entry.shcool_faculty.value;
-	const school_graduate_y = document.entry.school_graduate_y.value;
-	const school_graduate_m = document.entry.school_graduate_m.value;
+	const school_graduate_month = document.entry.school_graduate_month.value;
 	const qualification = document.entry.qualification.value;
 	const motivation = document.entry.motivation.value;
 	const question = document.entry.question.value;
 
 	const t_name = document.getElementById("t_name");
 	const t_name_kn = document.getElementById("t_name_kn");
-	const t_birth_ymd = document.getElementById("t_birth_ymd");
+	const t_birth_date = document.getElementById("t_birth_date");
 	const t_gender = document.getElementById("t_gender");
 	const t_address_code = document.getElementById("t_address_code");
 	const t_address_ken = document.getElementById("t_address_ken");
@@ -31,14 +28,14 @@ document.getElementById("button_go_verification").onclick = function() {
 	const t_email = document.getElementById("t_email");
 	const t_school_name = document.getElementById("t_school_name");
 	const t_shcool_faculty = document.getElementById("t_shcool_faculty");
-	const t_school_graduate_ym = document.getElementById("t_school_graduate_ym");
+	const t_school_graduate_month = document.getElementById("t_school_graduate_month");
 	const t_qualification = document.getElementById("t_qualification");
 	const t_motivation = document.getElementById("t_motivation");
 	const t_question = document.getElementById("t_question");
 
 	if (name) t_name.innerHTML = name;
 	if (name_kn) t_name_kn.innerHTML = name_kn;
-	if (birth_y && birth_m && birth_d) t_birth_ymd.innerHTML = birth_y + "/" + birth_m + "/" + birth_d;
+	if (birth_date) t_birth_date.innerHTML = birth_date;
 	if (gender) t_gender.innerHTML = gender;
 	if (address_code) t_address_code.innerHTML = address_code;
 	if (address_ken) t_address_ken.innerHTML = address_ken;
@@ -48,7 +45,7 @@ document.getElementById("button_go_verification").onclick = function() {
 	if (email) t_email.innerHTML = email;
 	if (school_name) t_school_name.innerHTML = school_name;
 	if (shcool_faculty) t_shcool_faculty.innerHTML = shcool_faculty;
-  if (school_graduate_y && school_graduate_m) t_school_graduate_ym.innerHTML = school_graduate_y + "/" + school_graduate_m;
+  if (school_graduate_month) t_school_graduate_month.innerHTML = school_graduate_month;
 	if (qualification) t_qualification.innerHTML = qualification;
 	if (motivation) t_motivation.innerHTML = motivation;
   if (question) t_question.innerHTML = question;
@@ -58,5 +55,4 @@ document.getElementById("button_go_verification").onclick = function() {
 
   form_input.classList.add("d-none");
   form_verification.classList.remove("d-none");
-
 };
