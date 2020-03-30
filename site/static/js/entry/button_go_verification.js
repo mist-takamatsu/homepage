cc2cb7b37593b048841e26b7318f5a1d9ac943b1
gnshd <gdev@nshd.info>
2020-03-30 17:06:28 +0900

    Update

--- site/static/js/entry/button_go_verification.js
+++ site/static/js/entry/button_go_verification.js
@@ -150,3 +150,28 @@ document.getElementById("button_go_verification").onclick = function () {
     document.getElementById("feedback_message").classList.remove("d-none");
   };
 };
+
+document.getElementById("email").onkeyup = function() {
+	ConfiremEmail();
+};
+
+document.getElementById("email_confirem").onkeyup = function() {
+	ConfiremEmail();
+};
+
+function ConfiremEmail() {
+	const email = document.entry.email.value;
+	const email_confirem = document.entry.email_confirem.value;
+	const feedback_email = document.getElementById("feedback_email");
+	const feedback_email_message = document.getElementById(
+		"feedback_email_message"
+	);
+
+	if (email_confirem && email !== email_confirem) {
+		feedback_email_message.innerHTML = "メールアドレスが一致していません";
+		feedback_email.classList.remove("d-none");
+	} else {
+		feedback_email_message.innerHTML = "メールアドレスは必須です";
+		feedback_email.classList.add("d-none");
+	}
+}
--- site/static/js/entry/defult.js
+++ site/static/js/entry/defult.js
@@ -4,29 +4,3 @@ function submit_cancel() {
 		return false;
 	}
 }
-
-
-document.getElementById("email").onkeyup = function() {
-	ConfiremEmail();
-};
-
-document.getElementById("email_confirem").onkeyup = function() {
-	ConfiremEmail();
-};
-
-function ConfiremEmail() {
-	const email = document.entry.email.value;
-	const email_confirem = document.entry.email_confirem.value;
-	const feedback_email = document.getElementById("feedback_email");
-	const feedback_email_message = document.getElementById(
-		"feedback_email_message"
-	);
-
-	if (email_confirem && email !== email_confirem) {
-		feedback_email_message.innerHTML = "メールアドレスが一致していません";
-		feedback_email.classList.remove("d-none");
-	} else {
-		feedback_email_message.innerHTML = "メールアドレスは必須です";
-		feedback_email.classList.add("d-none");
-	}
-}
