9eb630f875608737db4edeca68d40df706fc35de
gnshd <gdev@nshd.info>
2020-06-19 10:11:03 +0900

    Update entry pc

--- site/static/js/entry/button_back_input.js
+++ site/static/js/entry/button_back_input.js
@@ -3,9 +3,10 @@ document.getElementById("button_back_input").onclick = function() {
   const title = document.getElementById("title");
   title.innerHTML = "エントリーホーム";
 
-	const pager_verification = document.getElementById("pager_verification");
-	pager_verification.classList.add("circle");
-  pager_verification.classList.remove("line-circle");
+  const pager_verification = document.getElementById("pager_verification");
+  pager_verification.classList.remove("circle");
+	pager_verification.classList.add("line-circle");
+
 
   const form_input = document.getElementById("form_input");
   const form_verification = document.getElementById("form_verification");
